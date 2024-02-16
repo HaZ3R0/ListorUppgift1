c486b0bbe1453338400034d9ff36a036186f31d2
HaZ3R0 <megamackan@outlook.com>
2024-02-16 12:24:25 +0100

    Add project files.

---
+++ ListorUppgift1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListorUppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namnen = new List<string>();
+            Console.WriteLine("Skriv in ett namn");
+            string namn = Console.ReadLine();
+            namnen.Add(namn);
+            while(namn!= "")
+            {
+                Console.WriteLine("Skriv in ett namn till. Om du känner dig klar klicka bara enter");
+                namn = Console.ReadLine();
+                namnen.Add(namn);
+            }
+
+            Console.WriteLine("Här är namnen du skrev in");
+            namnen.Reverse();
+
+            foreach (string namn1 in namnen)
+            {
+                Console.WriteLine(namn1);
+            }
+        }
+    }
+}
